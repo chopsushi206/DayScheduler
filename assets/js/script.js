c3568c1909943a1d7d07f640e1d987b76c44d2b2
LiHua Anderson <lihua.anderson@gmail.com>
2021-03-04 17:10:06 -0800

    working on if statement to change background colors of each of the text areas depending on the moment.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,12 @@
 
 const now = moment().format('dddd MMMM Do YYYY');
-const currentHour = moment().format('h');
+const currentHour = moment().format('H');
 
 console.log(currentHour);
 
-const $currentDate = $('#currentDay');
-$currentDate.text(now);
 
-/*let containerEl = $('.container');
-let scheduleRowEl = $('<span>');
-let scheduleInputEl = $('<input>');
-let saveEl = $('<div>');
-
-scheduleRowEl.text(currentHour);
-scheduleRowEl.attr('class', 'col-12 col-md-2');
-scheduleInputEl.attr('value', 'testing');
-$(".element").attr("id","scheduleInput");
-scheduleInputEl.addClass('col-12 col-md-8');
-saveEl.text('Save');
-saveEl.on('click', console.log(scheduleInputEl.value));
-containerEl.append(scheduleRowEl, scheduleInputEl, saveEl);*/
+$('row').each(function())
+    if (($this)dataset.hour === currentHour) {
+        set.child('description').addClass('present')
+    }
+}
