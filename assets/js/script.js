3aff4438e4f4c28d330d6863d5f3ead952e9bbeb
LiHua Anderson <lihua.anderson@gmail.com>
2021-03-04 18:13:07 -0800

    adding an if else statement to change the background of input field depending on status of moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,28 @@ const currentHour = moment().format('H');
 console.log(currentHour);
 
 
-$('row').each(function())
-    if (($this)dataset.hour === currentHour) {
-        set.child('description').addClass('present')
+//$('.row').each(function())
+//    if (($this)dataset.hour === currentHour) {
+//        set.child('description').addClass('present')
+//    }
+//}
+
+/*$('.row').each(function(){
+   console.log(this)
+var hour = $(this).attr("data-hour")
+    
+    if (hour === currentHour) {
+        $(this).addClass('present')
     }
-}
+})*/
+
+$('.row').each(function () {
+    var hour = $(this).attr("data-hour")
+    if (hour === currentHour) {
+        $(this).children('.description').addClass('present')
+    } else if (hour > currentHour) {
+        $(this).children('.description').addClass('past')
+    } else (hour < currentHour) {
+        $(this).children('.description').addClass('future')
+    }
+});
