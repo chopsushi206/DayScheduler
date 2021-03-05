280c39d66fcfd1bfc5792551a836b4792e26881b
LiHua Anderson <lihua.anderson@gmail.com>
2021-03-05 11:38:41 -0800

    added comments to javascript and working on function for clicking save button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,31 @@
-
+// variable to set moment to format Tuesday, March 1st 2021
 const now = moment().format('dddd MMMM Do YYYY');
+// variable to set moment in format of the hour, in a 24 hour span. (01-24)
 const currentHour = moment().format('HH');
-
-console.log(currentHour);
-
+// variable $currentDate equal to currentDay
 const $currentDate = $('#currentDay');
+// sets the text of currentDate to ID currentDay in index.html
 $currentDate.text(now);
 
+// function for each div with class of row
 $('.row').each(function () {
-    var hour = $(this).attr("data-hour")
-    if (hour === currentHour) {
+    // hour is equal to this rows dataset.hour
+    let $hour = $(this).attr("data-hour")
+    // if both variables equal
+    if ($hour === currentHour) {
+        // add class of present to this rows child with class of description
         $(this).children('.description').addClass('present')
-    } else if (hour > currentHour) {
+        // if hour is greater than current hour add class of future to this rows child with class of description
+    } else if ($hour > currentHour) {
         $(this).children('.description').addClass('future')
-    } else if (hour < currentHour) {
+        // if hour is less than current hour add class of past to this rows child with class of description
+    } else if ($hour < currentHour) {
         $(this).children('.description').addClass('past')
     }
 });
+
+$('.row').each(function () {
+    let $saveBtnEl = $(this).children('saveBtn')
+    let $scheduleInfo = $(this).children('description').val
+    $saveBtnEl.click(console.log($scheduleInfo))
+})
