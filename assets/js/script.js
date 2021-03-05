ca57c1867e8bc63fb94465c2b87668d92b8b17f3
LiHua Anderson <lihua.anderson@gmail.com>
2021-03-04 18:47:50 -0800

    added back the two lines of code to display the date beneath the header. was accidentally deleted

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ const currentHour = moment().format('HH');
 
 console.log(currentHour);
 
+const $currentDate = $('#currentDay');
+$currentDate.text(now);
+
 $('.row').each(function () {
     var hour = $(this).attr("data-hour")
     if (hour === currentHour) {
