1ea735a1a943be75861b5ff95f5fe38b92bc9a17
LiHua Anderson <lihua.anderson@gmail.com>
2021-03-04 16:41:23 -0800

    removed some script from script.JS and added a lot of elements to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-// create a variable for now
-// create a bunch of inputs for each hour of the day
-// create a save button on the right of each hour
-// on click button should push input contents into array
-// on pageload should pull saved contents from array and display them within correct hour
-// current hour of day input should be red
-// passed time input should be grey
-// future time blocks should appear green
 
 const now = moment().format('dddd MMMM Do YYYY');
 const currentHour = moment().format('h');
@@ -15,7 +7,16 @@ console.log(currentHour);
 const $currentDate = $('#currentDay');
 $currentDate.text(now);
 
-let containerEl = $('.container');
-let scheduleRowEl = $('<div>');
-scheduleRowEl.text = $(currentHour);
-containerEl.append(scheduleRowEl);
+/*let containerEl = $('.container');
+let scheduleRowEl = $('<span>');
+let scheduleInputEl = $('<input>');
+let saveEl = $('<div>');
+
+scheduleRowEl.text(currentHour);
+scheduleRowEl.attr('class', 'col-12 col-md-2');
+scheduleInputEl.attr('value', 'testing');
+$(".element").attr("id","scheduleInput");
+scheduleInputEl.addClass('col-12 col-md-8');
+saveEl.text('Save');
+saveEl.on('click', console.log(scheduleInputEl.value));
+containerEl.append(scheduleRowEl, scheduleInputEl, saveEl);*/
